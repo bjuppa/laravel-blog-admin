0bff93af8513032fa3d956f66e66b8ffa1d08792
Björn Nilsved <nilsved@gmail.com>
2018-10-15 11:49:02 +0200

    Add configuration, routes and resources for the package

--- src/BlogAdminServiceProvider.php
+++ src/BlogAdminServiceProvider.php
@@ -3,15 +3,18 @@
 namespace Bjuppa\LaravelBlogAdmin;
 
 use Illuminate\Support\ServiceProvider;
+use Kontenta\Kontour\Concerns\RegistersAdminRoutes;
 
 class BlogAdminServiceProvider extends ServiceProvider
 {
+    use RegistersAdminRoutes;
+
     /**
      * Register bindings in the container.
      */
     public function register()
     {
-        //
+        $this->configure();
     }
 
     /**
@@ -20,6 +23,33 @@ class BlogAdminServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerAdminRoutes(__DIR__ . '/../routes/blog-admin.php');
+
+        $this->registerResources();
+    }
+
+    /**
+     * Setup the configuration.
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/blog-admin.php', 'blog-admin');
+
+        // Ensure default config values are set for those that are used in two or more places
+        if (empty(config('blog-admin.view_namespace'))) {
+            config(['blog-admin.view_namespace' => 'blog-admin']);
+        }
+        if (empty(config('blog-admin.trans_namespace'))) {
+            config(['blog-admin.trans_namespace' => 'blog-admin']);
+        }
+    }
+
+    /**
+     * Register the resources.
+     */
+    protected function registerResources()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', config('blog-admin.view_namespace'));
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', config('blog-admin.trans_namespace'));
     }
 }
