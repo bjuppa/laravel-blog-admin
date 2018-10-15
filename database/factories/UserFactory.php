a938c4675c8eceb43b28cbb11427393bba23d627
Björn Nilsved <nilsved@gmail.com>
2018-10-15 14:57:55 +0200

    Add a fake user and factory

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(Bjuppa\LaravelBlogAdmin\Tests\Feature\Fakes\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
---
+++ tests/Feature/Fakes/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bjuppa\LaravelBlogAdmin\Tests\Feature\Fakes;
+
+use Kontenta\Kontour\Auth\AdminUser;
+
+class User extends AdminUser
+{
+
+}
