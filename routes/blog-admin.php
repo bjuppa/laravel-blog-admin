3ce228b2c1413357a34f3ff090ba0e0e467de32e
Björn Nilsved <nilsved@gmail.com>
2018-10-26 09:50:57 +0200

    Add update route

--- routes/blog-admin.php
+++ routes/blog-admin.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Bjuppa\LaravelBlogAdmin\Http\Controllers')->name('blog-admin.')->group(function () {
     Route::get('/blogs/{id}', 'BlogController@show')->name('blogs.show');
     Route::get('/blog-entries/{id}/edit', 'EntryController@edit')->name('entries.edit');
+    Route::put('/blog-entries/{id}', 'EntryController@update')->name('entries.update');
 });
