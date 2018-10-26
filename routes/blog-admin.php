c40d5fac5abaa983fc9db86a2b2396d28427db62
Björn Nilsved <nilsved@gmail.com>
2018-10-26 10:18:33 +0200

    Add "create" to create path

--- routes/blog-admin.php
+++ routes/blog-admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Bjuppa\LaravelBlogAdmin\Http\Controllers')->name('blog-admin.')->group(function () {
     Route::get('/blogs/{id}', 'BlogController@show')->name('blogs.show');
-    Route::get('/blogs/{id}/entries', 'EntryController@create')->name('entries.create');
+    Route::get('/blogs/{id}/entries/create', 'EntryController@create')->name('entries.create');
     Route::post('/blog-entries', 'EntryController@store')->name('entries.store');
     Route::get('/blog-entries/{id}/edit', 'EntryController@edit')->name('entries.edit');
     Route::put('/blog-entries/{id}', 'EntryController@update')->name('entries.update');
