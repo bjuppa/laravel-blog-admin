f8f826dc6b10f796d512fbe38c6298201d34a4b3
Björn Nilsved <nilsved@gmail.com>
2018-11-12 15:53:02 +0100

    Display errors for fields

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -1,5 +1,20 @@
 <label for="entry.title">Title</label>
-<input id="entry.title" type="text" name="title" value="{{ old('title', $entry->title) }}" required>
+<input id="entry.title" type="text" name="title" value="{{ old('title', $entry->title) }}" required @if($errors->has('title')) aria-invalid="true" aria-describedby="entry.title.errors" @endif>
+@if($errors->has('title'))
+<ul id="entry.title.errors">
+  @foreach($errors->get('title') as $message)
+    <li>{{ $message }}</li>
+  @endforeach
+</ul>
+@endif
 
 <label for="entry.content">Content</label>
-<textarea id="entry.content" name="content" required>{{ old('content', $entry->content) }}</textarea>
+<textarea id="entry.content" name="content" required @if($errors->has('content')) aria-invalid="true" aria-describedby="entry.content.errors" @endif>{{ old('content', $entry->content) }}</textarea>
+@if($errors->has('content'))
+<ul id="entry.content.errors">
+  @foreach($errors->get('content') as $message)
+    <li>{{ $message }}</li>
+  @endforeach
+</ul>
+@endif
+
