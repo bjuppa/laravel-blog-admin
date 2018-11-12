5b105fa8c510eeeb7d51abdea3aea3defe1d71e2
Björn Nilsved <nilsved@gmail.com>
2018-11-12 16:30:40 +0100

    Wrap inputs in divs for positioning below labels

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -1,4 +1,5 @@
 <label for="entry.title">Title</label>
+<div>
 <input id="entry.title" type="text" name="title" value="{{ old('title', $entry->title) }}" required @if($errors->has('title')) aria-invalid="true" aria-describedby="entry.title.errors" @endif>
 @if($errors->has('title'))
 <ul id="entry.title.errors">
@@ -7,8 +8,10 @@
   @endforeach
 </ul>
 @endif
+</div>
 
 <label for="entry.content">Content</label>
+<div>
 <textarea id="entry.content" name="content" required @if($errors->has('content')) aria-invalid="true" aria-describedby="entry.content.errors" @endif>{{ old('content', $entry->content) }}</textarea>
 @if($errors->has('content'))
 <ul id="entry.content.errors">
@@ -17,4 +20,4 @@
   @endforeach
 </ul>
 @endif
-
+</div>
