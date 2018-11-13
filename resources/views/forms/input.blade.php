f94d3b003d4a93e78a2ba14430980462822f3456
Björn Nilsved <nilsved@gmail.com>
2018-11-13 16:59:14 +0100

    Make the label accept html

--- resources/views/forms/input.blade.php
+++ resources/views/forms/input.blade.php
@@ -1,4 +1,4 @@
-<label for="{{ $controlId = $controlId ?? (($idPrefix ?? '') . $name) }}">{{ $label ?? $name }}</label>
+<label for="{{ $controlId = $controlId ?? (($idPrefix ?? '') . $name) }}">{!! $label ?? $name !!}</label>
 <div>
 <input type="{{ $type ?? 'text' }}" value="{{ old($name, $value ?? $slot ?? isset($model) ? $model->$name : '') }}"
   @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . 'Errors'])
