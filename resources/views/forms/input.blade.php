ca3965427c1458157136bfe8c3529361edd64c08
Björn Nilsved <nilsved@gmail.com>
2018-11-13 16:54:57 +0100

    Format code

--- resources/views/forms/input.blade.php
+++ resources/views/forms/input.blade.php
@@ -1,8 +1,6 @@
 <label for="{{ $controlId = $controlId ?? (($idPrefix ?? '') . $name) }}">{{ $label ?? $name }}</label>
 <div>
-<input
-  type="{{ $type ?? 'text' }}"
-  value="{{ old($name, $value ?? $slot ?? isset($model) ? $model->$name : '') }}"
+<input type="{{ $type ?? 'text' }}" value="{{ old($name, $value ?? $slot ?? isset($model) ? $model->$name : '') }}"
   @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => ($errorsId = $controlId . 'Errors')])
 >
 @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
