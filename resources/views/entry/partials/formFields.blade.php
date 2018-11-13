e9b3f41ebab40f221e3ef3dd322d8363c37d2cda
Björn Nilsved <nilsved@gmail.com>
2018-11-13 16:29:08 +0100

    Create input partial

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -1,14 +1,4 @@
-<label for="entry.title">Title</label>
-<div>
-<input id="entry.title" type="text" name="title" value="{{ old('title', $entry->title) }}" required @if($errors->has('title')) aria-invalid="true" aria-describedby="entry.title.errors" @endif>
-@if($errors->has('title'))
-<ul id="entry.title.errors">
-  @foreach($errors->get('title') as $message)
-    <li>{{ $message }}</li>
-  @endforeach
-</ul>
-@endif
-</div>
+@include('blog-admin::forms.input', ['name' => 'title'])
 
 <label for="entry.content">Content</label>
 <div>
---
+++ resources/views/forms/input.blade.php
@@ -0,0 +1,31 @@
+<label for="{{ $controlId = $controlId ?? (($idPrefix ?? '') . $name) }}">{{ $label ?? $name }}</label>
+<div>
+<input
+  type="{{ $type ?? 'text' }}"
+  id="{{ $controlId }}"
+  value="{{ old($name, $value ?? $slot ?? isset($model) ? $model->$name : '') }}"
+  name="{{ $name }}"
+  @if($errors->has($name))
+    aria-invalid="true"
+    aria-describedby="{{ $errorId = $controlId . '.errors' }}"
+  @elseif(isset($ariaDescribedById))
+    aria-describedby="{{ $ariaDescribedById }}"
+  @endif
+  @if(isset($controlAttributes))
+    @if(is_array($controlAttributes))
+      @foreach($controlAttributes as $attributeName => $attributeValue)
+        {{ attributeName }}="{{ $attributeValue }}"
+      @endforeach
+    @else
+      {!! $controlAttributes !!}
+    @endif
+  @endif
+>
+@if($errors->has($name))
+<ul id="{{ $errorId }}">
+  @foreach($errors->get($name) as $message)
+    <li>{{ $message }}</li>
+  @endforeach
+</ul>
+@endif
+</div>
