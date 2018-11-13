e4e9bb0b55009e4a058533d9514779df1dd4646c
Björn Nilsved <nilsved@gmail.com>
2018-11-13 17:17:49 +0100

    Create a textarea partial

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -1,13 +1,2 @@
 @include('blog-admin::forms.input', ['name' => 'title', 'label' => 'Title', 'controlAttributes' => ['required']])
-
-<label for="entry.content">Content</label>
-<div>
-<textarea id="entry.content" name="content" required @if($errors->has('content')) aria-invalid="true" aria-describedby="entry.content.errors" @endif>{{ old('content', $entry->content) }}</textarea>
-@if($errors->has('content'))
-<ul id="entry.content.errors">
-  @foreach($errors->get('content') as $message)
-    <li>{{ $message }}</li>
-  @endforeach
-</ul>
-@endif
-</div>
+@include('blog-admin::forms.textarea', ['name' => 'content', 'label' => 'Content', 'controlAttributes' => ['required']])
