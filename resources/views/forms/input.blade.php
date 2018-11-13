bfc52c533e41a52b10eb73ec4cc5038f2e2e3f01
Björn Nilsved <nilsved@gmail.com>
2018-11-13 17:04:15 +0100

    Make errors element id suffix configurable

--- resources/views/forms/input.blade.php
+++ resources/views/forms/input.blade.php
@@ -1,7 +1,7 @@
 <label for="{{ $controlId = $controlId ?? (($idPrefix ?? '') . $name) }}">{!! $label ?? $name !!}</label>
 <div>
 <input type="{{ $type ?? 'text' }}" value="{{ old($name, $value ?? $slot ?? isset($model) ? $model->$name : '') }}"
-  @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . 'Errors'])
+  @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
 >
 @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
 </div>
