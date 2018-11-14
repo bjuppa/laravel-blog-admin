289376553d6b744112a5957b9be169983df331f9
Björn Nilsved <nilsved@gmail.com>
2018-11-14 15:59:05 +0100

    Use translations for labels and fallback to a humanized name conversion

--- resources/views/forms/partials/label.blade.php
+++ resources/views/forms/partials/label.blade.php
@@ -1,1 +1,1 @@
-<label for="{{ $controlId }}">{!! $label ?? $name !!}</label>
+<label for="{{ $controlId }}">{!! $label ?? ucfirst(Lang::has('validation.attributes.'.$name) ? Lang::trans('validation.attributes.'.$name) : str_replace('_', ' ', $name)) !!}</label>
