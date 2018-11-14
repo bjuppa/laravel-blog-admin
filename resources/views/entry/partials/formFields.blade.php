b6b7653ee36947144bbadd283186a80b1e249df1
Björn Nilsved <nilsved@gmail.com>
2018-11-14 15:29:08 +0100

    Add publish_after input

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -1,2 +1,3 @@
 @include('blog-admin::forms.input', ['name' => 'title', 'label' => 'Title', 'controlAttributes' => ['required']])
+@include('blog-admin::forms.input', ['name' => 'publish_after', 'type' => 'datetime-local', 'label' => 'Publish after'])
 @include('blog-admin::forms.textarea', ['name' => 'content', 'label' => 'Content', 'controlAttributes' => ['required']])
