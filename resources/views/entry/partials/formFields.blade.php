9d4535bb04b6e044b7283de00764f3e606f9eb02
Björn Nilsved <nilsved@gmail.com>
2018-11-14 17:32:00 +0100

    Add all editable fields

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -2,4 +2,12 @@
 @include('blog-admin::forms.input', ['name' => 'slug'])
 @include('blog-admin::forms.input', ['name' => 'publish_after', 'type' => 'datetime-local'])
 @include('blog-admin::forms.input', ['name' => 'author_name'])
+@include('blog-admin::forms.input', ['name' => 'author_email'])
+@include('blog-admin::forms.input', ['name' => 'author_url'])
+@include('blog-admin::forms.textarea', ['name' => 'image'])
 @include('blog-admin::forms.textarea', ['name' => 'content', 'controlAttributes' => ['required']])
+@include('blog-admin::forms.textarea', ['name' => 'summary'])
+@include('blog-admin::forms.input', ['name' => 'page_title'])
+@include('blog-admin::forms.input', ['name' => 'description'])
+@include('blog-admin::forms.textarea', ['name' => 'json_meta_tags'])
+<pre>{{ $entry->getMetaTagBag()->toHtml() }}</pre>
