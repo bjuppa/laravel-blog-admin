e7a4c1696d105f0321509740cb79b6985960d467
Björn Nilsved <nilsved@gmail.com>
2018-11-14 14:24:16 +0100

    Get model values using array syntax

--- resources/views/forms/input.blade.php
+++ resources/views/forms/input.blade.php
@@ -1,6 +1,7 @@
 @include('blog-admin::forms.partials.label', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name)])
 <div>
-<input type="{{ $type ?? 'text' }}" value="{{ old($name, $value ?? $slot ?? isset($model) ? $model->$name : '') }}"
+<input type="{{ $type ?? 'text' }}"
+  value="{{ old($name, $value ?? $slot ?? isset($model[$name]) ? $model[$name] : '') }}"
   @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
 >
 @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
