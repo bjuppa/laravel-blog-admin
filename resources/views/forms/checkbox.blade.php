0c973c77a30045a4033b4916b50572e3522c707d
Björn Nilsved <nilsved@gmail.com>
2018-11-15 10:50:17 +0100

    Use label as component

--- resources/views/forms/checkbox.blade.php
+++ resources/views/forms/checkbox.blade.php
@@ -1,10 +1,12 @@
 <div>
-  <label><input type="checkbox"
-    @if(old($name, $checked ?? $model[$name] ?? false))
-      checked
-    @endif
-    value="{{ $value ?? $checkbox_default_value ?? '1' }}"
-    @include('blog-admin::forms.partials.inputAttributes', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
-    >{!! $label ?? ucfirst(Lang::has('validation.attributes.'.$name) ? Lang::trans('validation.attributes.'.$name) : str_replace('_', ' ', $name)) !!}</label>
+  @component('blog-admin::forms.partials.label', compact('name'))
+    <input type="checkbox"
+      @if(old($name, $checked ?? $model[$name] ?? false))
+        checked
+      @endif
+      value="{{ $value ?? $checkbox_default_value ?? '1' }}"
+      @include('blog-admin::forms.partials.inputAttributes', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
+    >
+    @endcomponent
     @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
 </div>
