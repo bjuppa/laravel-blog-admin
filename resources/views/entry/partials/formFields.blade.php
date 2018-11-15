2c4805513eac62d93b3fec80095ded90d53903fd
Björn Nilsved <nilsved@gmail.com>
2018-11-15 08:51:51 +0100

    Hide some fields until entry has been created

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -1,14 +1,24 @@
 @include('blog-admin::forms.input', ['name' => 'title', 'controlAttributes' => ['required']])
-@include('blog-admin::forms.input', ['name' => 'slug'])
+
+@if($entry->exists)
+  @include('blog-admin::forms.input', ['name' => 'slug'])
+@endif
+
 @include('blog-admin::forms.input', ['name' => 'publish_after', 'type' => 'datetime-local'])
+
 @include('blog-admin::forms.input', ['name' => 'author_name'])
 @include('blog-admin::forms.input', ['name' => 'author_email'])
 @include('blog-admin::forms.input', ['name' => 'author_url'])
+
 @include('blog-admin::forms.textarea', ['name' => 'image'])
 {{ $entry->getImage() }}
+
 @include('blog-admin::forms.textarea', ['name' => 'content', 'controlAttributes' => ['required']])
-@include('blog-admin::forms.textarea', ['name' => 'summary'])
-@include('blog-admin::forms.input', ['name' => 'page_title'])
-@include('blog-admin::forms.input', ['name' => 'description'])
-@include('blog-admin::forms.textarea', ['name' => 'json_meta_tags'])
-<pre>{{ $blog->getDefaultMetaTags()->merge($entry)->toHtml() }}</pre>
+
+@if($entry->exists)
+  @include('blog-admin::forms.textarea', ['name' => 'summary'])
+  @include('blog-admin::forms.input', ['name' => 'page_title'])
+  @include('blog-admin::forms.input', ['name' => 'description'])
+  @include('blog-admin::forms.textarea', ['name' => 'json_meta_tags'])
+  <pre>{{ $blog->getDefaultMetaTags()->merge($entry)->toHtml() }}</pre>
+@endif
