b399a9129d151bfa72aed264c99eb8c7e9f42b9a
Björn Nilsved <nilsved@gmail.com>
2018-11-15 10:54:53 +0100

    Allow echoing label start and end without escaping

--- resources/views/forms/partials/label.blade.php
+++ resources/views/forms/partials/label.blade.php
@@ -2,4 +2,4 @@
   @if(isset($controlId))
     for="{{ $controlId }}"
   @endif
->{{ $labelStart ?? '' }}{!! $label ?? ucfirst(Lang::has('validation.attributes.'.$name) ? Lang::trans('validation.attributes.'.$name) : str_replace('_', ' ', $name)) !!}{{ $labelEnd ?? '' }}</{{ $labelTag }}>
+>{!! $labelStart ?? '' !!}{!! $label ?? ucfirst(Lang::has('validation.attributes.'.$name) ? Lang::trans('validation.attributes.'.$name) : str_replace('_', ' ', $name)) !!}{!! $labelEnd ?? '' !!}</{{ $labelTag }}>
