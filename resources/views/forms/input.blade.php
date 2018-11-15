fd24317621b8b653590636e53af3abf502d84b05
Björn Nilsved <nilsved@gmail.com>
2018-11-15 11:07:11 +0100

    Format code

--- resources/views/forms/input.blade.php
+++ resources/views/forms/input.blade.php
@@ -1,8 +1,8 @@
 @include('blog-admin::forms.partials.label', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name)])
 <div>
-<input type="{{ $type ?? 'text' }}"
-  value="{{ old($name, $value ?? $slot ?? $model[$name] ?? '') }}"
-  @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
->
-@include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
+  <input type="{{ $type ?? 'text' }}"
+    value="{{ old($name, $value ?? $slot ?? $model[$name] ?? '') }}"
+    @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
+  >
+  @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
 </div>
--- resources/views/forms/textarea.blade.php
+++ resources/views/forms/textarea.blade.php
@@ -1,7 +1,7 @@
 @include('blog-admin::forms.partials.label', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name)])
 <div>
-<textarea
-  @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
->{{ old($name, $value ?? $slot ?? $model[$name] ?? '') }}</textarea>
-@include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
+  <textarea
+    @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
+  >{{ old($name, $value ?? $slot ?? $model[$name] ?? '') }}</textarea>
+  @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
 </div>
