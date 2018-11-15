92619a15ec90d9fa08bce3ae58cf9780ba7b3c8b
Björn Nilsved <nilsved@gmail.com>
2018-11-15 08:45:09 +0100

    Pull out the blog instance from registry for access in views

--- src/Http/Controllers/EntryController.php
+++ src/Http/Controllers/EntryController.php
@@ -2,21 +2,31 @@
 
 namespace Bjuppa\LaravelBlogAdmin\Http\Controllers;
 
-use Bjuppa\LaravelBlog\Eloquent\BlogEntry;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
 use Bjuppa\LaravelBlogAdmin\Http\Requests\StoreBlogEntry;
 use Bjuppa\LaravelBlogAdmin\Http\Requests\UpdateBlogEntry;
+use Bjuppa\LaravelBlog\Contracts\BlogRegistry;
+use Bjuppa\LaravelBlog\Eloquent\BlogEntry;
+use Illuminate\Routing\Controller as BaseController;
 
 //TODO: rename EntryController to BlogEntryController
 class EntryController extends BaseController
 {
-    public function create($blog)
-    {
-        $entry = new BlogEntry();
-        $entry->blog = $blog;
+    protected $blogRegistry;
 
-        return view('blog-admin::entry.create', compact('entry'));
+    public function __construct(BlogRegistry $blogRegistry)
+    {
+        $this->blogRegistry = $blogRegistry;
+    }
+
+    public function create($blog_id)
+    {
+        $blog = $this->blogRegistry->get($blog_id);
+        abort_unless($blog, 404, 'Blog "' . e($blog_id) . '" does not exist');
+
+        $entry = new BlogEntry();
+        $entry->blog = $blog->getId();
+
+        return view('blog-admin::entry.create', compact('entry', 'blog'));
     }
 
     public function store(StoreBlogEntry $request)
@@ -29,8 +39,9 @@ class EntryController extends BaseController
     public function edit($id)
     {
         $entry = BlogEntry::withUnpublished()->findOrFail($id);
+        $blog = $this->blogRegistry->get($entry->getBlogId());
 
-        return view('blog-admin::entry.edit', compact('entry'));
+        return view('blog-admin::entry.edit', compact('entry', 'blog'));
     }
 
     public function update(UpdateBlogEntry $request, $id)
