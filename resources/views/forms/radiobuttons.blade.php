dcd4d6f169ca4d23bc800297441fa42bc16c9206
Björn Nilsved <nilsved@gmail.com>
2018-11-15 12:06:39 +0100

    Implement radiobuttons

--- resources/views/forms/radiobuttons.blade.php
+++ resources/views/forms/radiobuttons.blade.php
@@ -1,4 +1,17 @@
 <fieldset>
   @include('blog-admin::forms.label', ['labelTag' => 'legend'])
-
+  @foreach($options as $option_value => $option_display)
+    @component('blog-admin::forms.label', ['label' => $option_display])
+      @slot('labelStart')
+        <input type="radio"
+          @if(old($name, $model[$name] ?? false) == $option_value)
+            checked
+          @endif
+          value="{{ $option_value }}"
+          @include('blog-admin::forms.partials.inputAttributes', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors'), 'controlId' => $controlId . $option_value])
+        >
+      @endslot
+    @endcomponent
+  @endforeach
+  @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
 </fieldset>
