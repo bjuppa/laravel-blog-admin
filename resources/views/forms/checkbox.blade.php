f6aee387f160dc4ab02cf11b02986c5e2cfb4f57
Björn Nilsved <nilsved@gmail.com>
2018-11-15 10:53:02 +0100

    Name the labelStart slot in label component

--- resources/views/forms/checkbox.blade.php
+++ resources/views/forms/checkbox.blade.php
@@ -1,12 +1,14 @@
 <div>
   @component('blog-admin::forms.partials.label', compact('name'))
-    <input type="checkbox"
-      @if(old($name, $checked ?? $model[$name] ?? false))
-        checked
-      @endif
-      value="{{ $value ?? $checkbox_default_value ?? '1' }}"
-      @include('blog-admin::forms.partials.inputAttributes', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
-    >
-    @endcomponent
-    @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
+    @slot('labelStart')
+      <input type="checkbox"
+        @if(old($name, $checked ?? $model[$name] ?? false))
+          checked
+        @endif
+        value="{{ $value ?? $checkbox_default_value ?? '1' }}"
+        @include('blog-admin::forms.partials.inputAttributes', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
+      >
+    @endslot
+  @endcomponent
+  @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
 </div>
