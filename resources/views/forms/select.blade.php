7cc6f3a4763e589412c0e664d8cd660209d276d4
Björn Nilsved <nilsved@gmail.com>
2018-11-27 17:34:28 +0100

    Add option groups

--- resources/views/forms/select.blade.php
+++ resources/views/forms/select.blade.php
@@ -5,12 +5,20 @@
       @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
     >
       @foreach($options as $option_value => $option_display)
+        @if($optgroup = is_array($option_display) ? $option_value : false)
+        <optgroup label="{{ $optgroup }}">
+        @endif
+        @foreach($optgroup ? $option_display : [$option_value => $option_display] as $option_value => $option_display)
         <option
           @if($selected === $option_value)
             selected
           @endif
           value="{{ $option_value }}"
         >{!! $option_display !!}</option>
+        @endforeach
+        @if($optgroup)
+        </optgroup>
+        @endif
       @endforeach
     </select>
     @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
