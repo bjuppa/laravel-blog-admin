dc36732f312db3dc4b25246927d7c00907a19568
Björn Nilsved <nilsved@gmail.com>
2018-11-28 13:01:30 +0100

    Set labels' for attribute on checkboxes and radios

--- resources/views/forms/checkbox.blade.php
+++ resources/views/forms/checkbox.blade.php
@@ -1,12 +1,12 @@
 <div>
-  @component('blog-admin::forms.label', ['name' => $name, 'label' => $label ?? null])
+  @component('blog-admin::forms.label', ['name' => $name, 'label' => $label ?? null, 'controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name)])
     @slot('labelStart')
       <input type="checkbox"
         @if(old($name, $checked ?? $model[$name] ?? false))
           checked
         @endif
         value="{{ $value ?? $checkbox_default_value ?? '1' }}"
-        @include('blog-admin::forms.partials.inputAttributes', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
+        @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
       >
     @endslot
   @endcomponent
--- resources/views/forms/radiobuttons.blade.php
+++ resources/views/forms/radiobuttons.blade.php
@@ -1,18 +1,20 @@
-<fieldset data-checked-radio="{{ $selected = strval(old($name, $selected ?? $model[$name] ?? '')) }}">
+<fieldset id="{{ $groupId = $controlId ?? (($idPrefix ?? '') . $name) }}"
+  data-checked-radio="{{ $selected = strval(old($name, $selected ?? $model[$name] ?? '')) }}"
+>
   @include('blog-admin::forms.label', ['labelTag' => 'legend'])
   @foreach($options as $option_value => $option_display)
     @if($legend = is_iterable($option_display) ? $option_value : false)
     <fieldset><legend>{{ $legend }}</legend>
     @endif
     @foreach($legend ? $option_display : [$option_value => $option_display] as $option_value => $option_display)
-    @component('blog-admin::forms.label', ['label' => $option_display])
+    @component('blog-admin::forms.label', ['label' => $option_display, 'controlId' => $controlId = $groupId . '[' . $option_value . ']'])
       @slot('labelStart')
         <input type="radio"
           @if($selected == strval($option_value))
             checked
           @endif
           value="{{ $option_value }}"
-          @include('blog-admin::forms.partials.inputAttributes', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors'), 'controlId' => $controlId . $option_value])
+          @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $groupId . ($errorsSuffix ?? 'Errors')])
         >
       @endslot
     @endcomponent
