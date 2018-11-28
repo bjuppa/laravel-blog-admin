30104f0d18a2a1d1e4d2bbd01fdd310a47d917bf
Björn Nilsved <nilsved@gmail.com>
2018-11-28 16:55:40 +0100

    Use input control blade templates from kontour

--- resources/views/entry/partials/formFields.blade.php
+++ resources/views/entry/partials/formFields.blade.php
@@ -1,30 +1,30 @@
-@include('blog-admin::forms.input', ['name' => 'title', 'controlAttributes' => ['required']])
+@include('kontour::forms.input', ['name' => 'title', 'controlAttributes' => ['required']])
 
 @if($entry->exists)
-  @include('blog-admin::forms.select', ['name' => 'blog', 'options' => $blog_options])
-  @include('blog-admin::forms.input', ['name' => 'slug'])
-  @include('blog-admin::forms.input', ['name' => 'publish_after', 'type' => 'datetime-local'])
+  @include('kontour::forms.select', ['name' => 'blog', 'options' => $blog_options])
+  @include('kontour::forms.input', ['name' => 'slug'])
+  @include('kontour::forms.input', ['name' => 'publish_after', 'type' => 'datetime-local'])
 @endif
 
-@include('blog-admin::forms.textarea', ['name' => 'content', 'controlAttributes' => ['required']])
-@include('blog-admin::forms.radiobuttons', ['name' => 'display_full_content_in_feed', 'options' => ['' => 'Blog default', 1 => 'Display full content', 0 => 'Display summary or teaser with a link to read more']])
-@include('blog-admin::forms.textarea', ['name' => 'summary'])
-@include('blog-admin::forms.textarea', ['name' => 'image'])
+@include('kontour::forms.textarea', ['name' => 'content', 'controlAttributes' => ['required']])
+@include('kontour::forms.radiobuttons', ['name' => 'display_full_content_in_feed', 'options' => ['' => 'Blog default', 1 => 'Display full content', 0 => 'Display summary or teaser with a link to read more']])
+@include('kontour::forms.textarea', ['name' => 'summary'])
+@include('kontour::forms.textarea', ['name' => 'image'])
 <samp>{{ $entry->getImage() }}</samp>
 
 <fieldset>
   <legend>Author</legend>
-  @include('blog-admin::forms.input', ['name' => 'author_name'])
-  @include('blog-admin::forms.input', ['name' => 'author_email'])
-  @include('blog-admin::forms.input', ['name' => 'author_url'])
+  @include('kontour::forms.input', ['name' => 'author_name'])
+  @include('kontour::forms.input', ['name' => 'author_email'])
+  @include('kontour::forms.input', ['name' => 'author_url'])
 </fieldset>
 
 @if($entry->exists)
   <fieldset>
     <legend>Meta-data</legend>
-    @include('blog-admin::forms.input', ['name' => 'page_title'])
-    @include('blog-admin::forms.input', ['name' => 'description'])
-    @include('blog-admin::forms.textarea', ['name' => 'json_meta_tags'])
+    @include('kontour::forms.input', ['name' => 'page_title'])
+    @include('kontour::forms.input', ['name' => 'description'])
+    @include('kontour::forms.textarea', ['name' => 'json_meta_tags'])
     <pre><samp>{{ $blog->getDefaultMetaTags()->merge($entry)->toHtml() }}</samp></pre>
   </fieldset>
 @endif
--- resources/views/forms/textarea.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-  @include('blog-admin::forms.label', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name)])
-  <div>
-    <textarea
-      @include('blog-admin::forms.partials.inputAttributes', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
-    >{{ old($name, $value ?? $slot ?? $model[$name] ?? '') }}</textarea>
-    @include('blog-admin::forms.partials.errors', ['errorsId' => $errorsId])
-  </div>
-</div>
