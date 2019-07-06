f31066f97d521fdd8e170dc9580912fe1d00bb1c
Björn Nilsved <nilsved@gmail.com>
2019-07-06 12:04:09 +0200

    Use kontours time template

--- resources/views/entry/partials/publishStatusString.blade.php
+++ resources/views/entry/partials/publishStatusString.blade.php
@@ -1,8 +1,8 @@
 <?php $format = $format ?? 'D, M j, Y H:i T' ?>
 @if($entry->isPublic())
-  Public since {{ $blog->convertToBlogTimezone($entry->getPublished())->format($format) }}
+  Public since @include('kontour::elements.time', ['carbon' => $blog->convertToBlogTimezone($entry->getPublished()), 'format' => $format])
 @elseif($entry->getPublished()->isFuture())
-  Scheduled for publishing {{ $blog->convertToBlogTimezone($entry->getPublished())->format($format) }}
+  Scheduled for publishing @include('kontour::elements.time', ['carbon' => $blog->convertToBlogTimezone($entry->getPublished()), 'format' => $format])
 @else
   Not scheduled for publishing
 @endif
